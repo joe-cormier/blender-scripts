59a6c3bf457150f9c456302108ab594c7f693500
Joe Cormier <joemcormier@outlook.com>
2021-12-06 15:56:12 -0500

    update tracking script

--- object tracking/extractTrackData.py
+++ object tracking/extractTrackData.py
@@ -1,19 +1,12 @@
-import pandas as pd
-import numpy as np
-import bpy
-import os
-
-D = bpy.data
-
 """
 Export track x,y coordinates created from a 2D object tracking
 Creates a dataframe containing all tracks within the movieclips object
-coordinates are % of image widght / height
+coordinates are % of image width / height
 to get in units of length, multiply x-coordinates by image width in length units (meters)
 and y-coordinates by image height in length units
 """
 
-outputFolder= "<path>"
+outputFolder= '<path>'
 testName = 'test'
 
 for clip in D.movieclips:
@@ -21,19 +14,21 @@ for clip in D.movieclips:
     print(f'Clip Duration: {clip.frame_duration}')
     # create empty dataframe to fill with track coordinates
     df = pd.DataFrame.from_dict({'frame': list(range(0, clip.frame_duration))})
-    
+
     print('')
     for ob in clip.tracking.objects:
         print(f'Processing object {ob.name}')
+        if ob.name == 'Camera':
+            continue
         trackNum = 1
         for track in ob.tracks:
-            print(f'Processing track {trackNum}: {track.name}')          
-            for frame in list(range(0, clip.frame_duration)):
-                marker = track.markers.find_frame(frame)
+            print(f'Processing track {trackNum}: {track.name}')
+            for index, row in df.iterrows():
+                marker = track.markers.find_frame(row.frame)
                 if marker:
                     coords = marker.co.xy
-                    df.loc[frame, f'{ob.name} Track {trackNum} x'] = coords[0]
-                    df.loc[frame, f'{ob.name} Track {trackNum} y'] = coords[1]
+                    df.loc[index, f'{ob.name} Track {trackNum} x'] = coords[0]
+                    df.loc[index, f'{ob.name} Track {trackNum} y'] = coords[1]
 
                 else:
                     continue
@@ -42,6 +37,12 @@ for clip in D.movieclips:
             trackNum += 1 
 
 # save
-df = df.replace('', np.nan)
-df = df.dropna()
-df.to_csv(os.path.join(outputFolder, f'{testName}.csv'), index=False)
+#df = df.replace('', np.nan)
+#df = df.dropna()
+df.to_csv(os.path.join(outputFolder, f'{testName}_{clipCount}.csv'), index=False)
+
+# create file with info
+info = pd.DataFrame.from_records({'pixel_width': D.movieclips[0].size[0],
+                               'pixel_height': D.movieclips[0].size[1]}, index=[0])
+
+info.to_csv(os.path.join(outputFolder, f'{testName}_{clipCount}_info.csv'), index=False)
